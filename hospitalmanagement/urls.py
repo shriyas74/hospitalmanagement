01c78c35205dcddccccf5add361536d106e3cec9
Hp-user <batraguneet5@gmail.com>
2019-04-09 12:56:06 +0530

    main url file

--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 from manager_app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^',include('manager_app.urls')),
-    url(r'^signup/$',views.manager_info),
-    url(r'^signin/$',views.manager_info)
-    #url(r'^ss/',views.SignUp)
 
-]
+
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
